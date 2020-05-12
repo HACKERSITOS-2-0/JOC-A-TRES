ec86bfed93b6407fb7a03af7dad58b548a646ca3
JoanVila03 <60349567+JoanVila03@users.noreply.github.com>
2020-05-12 21:25:40 +0200

    Moviment personatge(encara no funciona)

--- JOC/scripts/Personatge.gd
+++ JOC/scripts/Personatge.gd
@@ -1,17 +1,21 @@
-extends Node2D
-var x = 0
-var y = 0
+extends KinematicBody2D
+var moviment_x = 0
+var moviment_y = 0
+var velocitat=100
 
+# warning-ignore:unused_argument
 func _process(delta):
 	look_at(get_global_mouse_position())
 	#provisional per provar els nivells i les collisions
 	if Input.is_action_pressed("ui_right"):
-		var x = 1
+		var moviment_x = 1
 	elif Input.is_action_pressed("ui_left"):
-		var x = -1
+		var moviment_x = -1
 	if Input.is_action_pressed("ui_down"):
-		var y = 1
+		var moviment_y = 1
 	elif Input.is_action_pressed("ui_up"):
-		var y = -1
-#	move_and_slide(Vector2(x,y))
+		var moviment_y = -1
+	var direccio=Vector2(moviment_x, moviment_y).normalized()
+#	set_global_position(Vector2(x, y)+get_global_position())
+	var moviment = move_and_collide(direccio*delta*velocitat)
 	
