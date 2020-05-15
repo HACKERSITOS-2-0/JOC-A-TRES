97c9ca4296d749b7e3951052158e67081671e037
TRIGFIRE <60349533+TRIGFIRE@users.noreply.github.com>
2020-05-15 20:10:07 +0200

    millora funcionament torreta
    he canviat la manera en que es movien el la torreta i el cos, quan estan en moviment, van en la mateixa direcció, i quan esta apuntant (parat) el cos no rota i només ho fa la torreta

--- JOC/scripts/enemics/tancenemic.gd
+++ JOC/scripts/enemics/tancenemic.gd
@@ -7,7 +7,7 @@ export (int) var radi_xoc
 
 export (PackedScene) var bala_tanc
 
-var vel:bool=true
+var en_moviment:bool=true
 var p_disparar:bool = false
 var objectiu = null
 var dins : bool = false
@@ -27,9 +27,10 @@ func _ready():
 
 
 func _process(delta):
+	var dir_actual_cano = Vector2(1, 0).rotated($cano.global_rotation)
 	var direccio_actual = Vector2(1, 0).rotated($cos.global_rotation)
 	var dir_objectiu = (obj.global_position - global_position).normalized()
-	$cos.global_rotation = direccio_actual.linear_interpolate(dir_objectiu, velocitat_rotacio * delta).angle()
+#	$cos.global_rotation = direccio_actual.linear_interpolate(dir_objectiu, velocitat_rotacio * delta).angle()
 	
 	
 	if dins:
@@ -37,26 +38,27 @@ func _process(delta):
 		desplacament /= obstacle.global_position.distance_to(global_position) / 50
 		dir_objectiu += desplacament
 		
-	if vel:
+	if en_moviment:
 		move_and_collide(dir_objectiu * velocitat * delta)
-
+		$cos.global_rotation = direccio_actual.linear_interpolate(dir_objectiu, velocitat_rotacio * delta).angle()
+		$cano.global_rotation = dir_actual_cano.linear_interpolate(dir_objectiu, velocitat_rotacio * delta).angle()
 		
 	if objectiu:
-		var direccio_actual_cano = Vector2(1, 0).rotated($cano.global_rotation)
-		var direccio_objectiu = (objectiu.global_position - global_position).normalized()
-		$cano.global_rotation = direccio_actual_cano.linear_interpolate(direccio_objectiu, vel_torreta * delta).angle()
+#		var direccio_actual_cano = Vector2(1, 0).rotated($cano.global_rotation)
+#		var direccio_objectiu = (objectiu.global_position - global_position).normalized()
+		$cano.global_rotation = dir_actual_cano.linear_interpolate(dir_objectiu, vel_torreta * delta).angle()
 		dispara()
 
 func _on_Visio_body_entered(body):
 	if body.name == "Personatge":
 		objectiu = body
-		vel = false
+		en_moviment = false
 
 
 func _on_Visio_body_exited(body):
 	if body == objectiu:
 		objectiu = null
-		vel = true
+		en_moviment = true
 
 
 func _on_area_xoc_area_entered(area):
