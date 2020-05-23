902dffb4baaead1867a2226522e8cc39e5885687
JoanVila03 <60349567+JoanVila03@users.noreply.github.com>
2020-05-23 14:17:16 +0200

    Musica afegida
    De moment hi ha la música dels menús i del nivell1. Després posaré els efectes de so.

--- JOC/scripts/inici.gd
+++ JOC/scripts/inici.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var g_variables = get_tree().get_root().get_node('GlobalVariables')
+#var g_variables = get_tree().get_root().get_node('GlobalVariables')
 export (PackedScene) var menu_nivells
 
 func _ready():
@@ -16,6 +16,6 @@ func _on_Levels_pressed():
 func _on_Quit_pressed():
 	get_tree().quit()
 
-func _on_Continue_pressed():
-	if g_variables.current_lvl == 1:
-		get_tree().change_scene("res://escenes/nivells/nivell 1.tscn")
+#func _on_Continue_pressed():
+#	if g_variables.current_lvl == 1:
+#	get_tree().change_scene("res://escenes/nivells/nivell 1.tscn")
