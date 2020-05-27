844469f914ce76ff8681c668af544061d879011a
Martí Mestre Faus <martimestre03@gmail.com>
2020-05-27 12:16:52 +0200

    Update ganeover.gd

--- JOC/escenes/ganeover.gd
+++ JOC/escenes/ganeover.gd
@@ -1,10 +1,8 @@
 extends CanvasLayer
 
 
-func _on_retry_pressed():
-	#ha de recarregar el nivell en questió.
-	pass
-	
+onready var g_variables = get_node('..').get_node('GlobalVariables')
+
 
 func _on_levels_pressed():
 	var player = AudioStreamPlayer.new()
@@ -13,3 +11,10 @@ func _on_levels_pressed():
 	player.play()
 	yield(get_tree().create_timer(0.7), "timeout")
 	get_tree().change_scene("res://escenes/menu_levels.tscn")
+
+
+func _on_RETRY_pressed():
+	if g_variables.current_lvl == 1:
+		get_tree().change_scene("res://escenes/nivells/nivell 1.tscn")
+	elif g_variables.current_lvl == 1:
+		get_tree().change_scene("res://escenes/nivells/nivell 2.tscn")
